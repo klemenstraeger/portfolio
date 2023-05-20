53fd97d3b0ac3195837779edaf3d8c6c60f9c555
Klemens Träger <traegerklemens@gmail.com>
2023-05-20 14:17:42 +0200

    added images

--- components/Projects.vue
+++ components/Projects.vue
@@ -118,6 +118,11 @@ const projects = [
     title: "EWO Ads Manager | 2022-2023",
     description:
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    images: [
+      "/img/ewo-login.png",
+      "/img/ewo-budgetmanager.png",
+      "/img/ewo-link-traffic.png",
+    ],
 
     usedTechnologies: [
       {
